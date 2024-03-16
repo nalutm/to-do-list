b56c281f9da5e59b8a7cfa56ecea014857cb14c4
Nayara Luiza <nayara.luiza21@gmail.com>
2024-03-16 11:13:48 -0300

    :boom: fix: Reverting modal validation

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -4,10 +4,7 @@ import { TaskContext } from "../contexts/task";
 
 function Modal() {
     const { editAction, cancelAction, edit, id, taskTitle } = useContext(TaskContext);
-    const [newTitle, setNewTitle] = useState(taskTitle);
-
-    console.log(newTitle);
-    console.log(taskTitle)
+    const [newTitle, setNewTitle] = useState(taskTitle);   
 
     const handleChange = (evt: ChangeEvent<HTMLInputElement>) => {
         setNewTitle(evt.target.value);
@@ -15,19 +12,15 @@ function Modal() {
 
 	const onSubmit = () => {
         if (!newTitle) {
-            console.log(newTitle, "string vazia")
-            console.log(taskTitle, "task title")
-            console.log(newTitle, "new Title")
             setNewTitle(taskTitle);
-            console.log(newTitle, "new Title em seguida do setNewTitle")
-            
             cancelAction("edit");
             return
         }
-        
-        edit({ title: newTitle, id: id, checked: false });
+        if (newTitle) {
+            edit({ title: newTitle, id: id, checked: false });
+            cancelAction("edit");
+        }
 		
-        cancelAction("edit");
 	}
 
     if (taskTitle === '') {
